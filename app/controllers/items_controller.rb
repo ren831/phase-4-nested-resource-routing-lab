73de7794db59ce9e1327440fb69ad6c7510264ca
ren831 <rene83120@gmail.com>
2022-08-09 13:31:00 -0700

    commit tree 6ab5cd117a713bd383768fb490872edfe14073ea

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find_by(id: params(:user_id))
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
